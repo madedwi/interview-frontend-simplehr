a22e7ba2c0c59a7eb0067aee7126db924f00a231
Dwi Asmara Putra <putra.device@gmail.com>
2023-06-11 15:55:52 +0800

    [Fix] auth

--- src/pages/_layout/Login.tsx
+++ src/pages/_layout/Login.tsx
@@ -1,12 +1,18 @@
+import { ThemeProvider } from '@mui/material'
 import React, { PropsWithChildren } from 'react'
+import { Theme } from './AdminTheme'
 
 type Props = PropsWithChildren
 
 const LoginLayout = ({children}: Props) => {
   return (
-    <>
-    {children}
-    </>
+    <ThemeProvider theme={Theme}>
+      <main className='grow' style={{
+        height: 'calc(100vh)'
+      }}>
+        {children}
+      </main>
+    </ThemeProvider>
   )
 }
 
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardContent, FormGroup, InputLabel, OutlinedInput, Paper, TextField, Typography } from '@mui/material'
+import { Box, Button, Card, CardContent, FormGroup, InputLabel, OutlinedInput, Paper, TextField, Typography, useTheme } from '@mui/material'
 import React, { useEffect } from 'react'
 import { NextPageWithLayout } from '../_app'
 import LoginLayout from '../_layout/Login'
@@ -8,6 +8,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { StyledFormControl } from '@/components/form/FormControl'
 import DisplayError from '@/components/form/ErrorField'
 import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 type Props = {}
 
@@ -19,6 +20,8 @@ const schema = yup.object({
 type FormData = yup.InferType<typeof schema>;
 
 const Login: NextPageWithLayout = (props: Props) => {
+  const theme = useTheme();
+  const router = useRouter();
   const { control, handleSubmit, formState: { errors } } = useForm<FormData>({
       defaultValues: {
         username: '',
@@ -34,45 +37,53 @@ const Login: NextPageWithLayout = (props: Props) => {
       redirect: false,
     });
 
-    console.log(response);
+    if(response?.status == 200){
+      router.replace('/admin');
+    }
     
   }
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-        <Paper className='w-[400px] p-5'>
-          <Typography variant='h4'>Login</Typography>
-          <div className="mt-5">
-            <form method="post" onSubmit={handleSubmit(submitHandler)}>
-              <StyledFormControl error={errors.username != undefined}  size='small'>
-                <InputLabel htmlFor="username">Username/email</InputLabel>
-                <Controller
-                    name='username'
-                    control={control}
-                    render={({ field }) => <OutlinedInput type='email' id='username' placeholder='user@domain.com' size='small' label='Username/email' {...field} /> }
-                  />
-                <DisplayError error={errors.username} />
-              </StyledFormControl>
+      <Paper sx={{
+        width: 600,
+        padding: theme.spacing(5),
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 'calc((100vh / 2) - 250px)'
+      }}>
+        <Typography variant='h2'>Login</Typography>
+        <Box sx={{
+          marginTop: theme.spacing(3)
+        }}>
+          <form method="post" onSubmit={handleSubmit(submitHandler)}>
+            <StyledFormControl error={errors.username != undefined}  size='small'>
+              <InputLabel htmlFor="username" shrink>Username/email</InputLabel>
+              <Controller
+                  name='username'
+                  control={control}
+                  render={({ field }) => <OutlinedInput type='email' notched id='username' placeholder='user@domain.com' size='small' label='Username/email' {...field} /> }
+                />
+              <DisplayError error={errors.username} />
+            </StyledFormControl>
 
-              <StyledFormControl error={errors.password != undefined}  size='small'>
-                <InputLabel htmlFor="password">Password</InputLabel>
-                <Controller
-                    name='password'
-                    control={control}
-                    render={({ field }) => <OutlinedInput type='password' id='password' label='password' size='small' {...field} /> }
-                  />
-                <DisplayError error={errors.password} />
-              </StyledFormControl>
+            <StyledFormControl error={errors.password != undefined}  size='small'>
+              <InputLabel htmlFor="password" shrink>Password</InputLabel>
+              <Controller
+                  name='password'
+                  control={control}
+                  render={({ field }) => <OutlinedInput notched type='password' id='password' label='password' size='small' {...field} /> }
+                />
+              <DisplayError error={errors.password} />
+            </StyledFormControl>
 
-              <FormGroup>
-                <Button fullWidth variant='contained' color='primary' type='submit'>Login</Button>
-              </FormGroup>
+            <FormGroup>
+              <Button fullWidth variant='contained' color='primary' type='submit'>Login</Button>
+            </FormGroup>
 
-            </form>
-          </div>
-        </Paper>
-    </main>
+          </form>
+        </Box>
+      </Paper>
   )
 }
 
